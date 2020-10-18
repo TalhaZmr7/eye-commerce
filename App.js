dda28647c490690b17d4170c2d8f0bcfc16a23b3
Talah Zameer <46329219+TalhaZmr7@users.noreply.github.com>
2020-10-18 20:43:51 +0500

    Added comment in App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {useState}  from 'react';
 import { StatusBar, StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './src/Routes';
-
+//This is the entry point of the application.
 export default function App() {
 
   return(
